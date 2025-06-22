5533ee70581c85ef588dcc94eac824d4cd28082e
guashasha <davidcarrionr00@hotmail.com>
2025-06-22 13:09:08 -0600

    fix: errors on reported posts, now works correctly
    calls to services weren't correct on reported posts controller and multimedia service manager

--- src/main/java/com/betterme/controllers/ReportedPostsController.java
+++ src/main/java/com/betterme/controllers/ReportedPostsController.java
@@ -17,6 +17,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
@@ -28,12 +29,14 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.ResourceBundle;
 
-public class ReportedPostsController {
+public class ReportedPostsController implements Initializable  {
     private final HttpClient client = HttpClient.newHttpClient();
     public ImageView imageArea;
     public Label titleLabel;
@@ -42,13 +45,15 @@ public class ReportedPostsController {
     private String reason = null;
     private Post post = null;
     private String postId = null;
+    private String reportId = null;
 
     private final String reportsUri = ProgramConfigurations.getConfiguration()
             .getProperty("reportsAPI.url");
     private final String postsUri = ProgramConfigurations.getConfiguration()
             .getProperty("postsAPI.url");
 
-    public ReportedPostsController() {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         getNextReportedPost();
 
         if (post != null) {
@@ -62,7 +67,7 @@ public class ReportedPostsController {
     private void setPostInfo() {
         titleLabel.setText(post.getTitle().trim());
         descriptionLabel.setText(post.getDescription().trim());
-        imageArea.setImage(new Image(new ByteArrayInputStream(post.getMultimedia())));
+        showImage();
     }
 
     private void resetPostInfo() {
@@ -71,11 +76,16 @@ public class ReportedPostsController {
         imageArea.setImage(null);
     }
 
+    private void showImage() {
+        Image image = new Image(new ByteArrayInputStream(post.getMultimedia()));
+        imageArea.setImage(image);
+    }
+
     private void getNextReportedPost() {
         HttpRequest reportRequest = HttpRequest.newBuilder()
                 .uri(URI.create(reportsUri))
                 .header("Content-Type", "application/json")
-                .header("Authorization", CurrentUser.jwt)
+                .header("Authorization", "Bearer " + CurrentUser.jwt)
                 .GET()
                 .build();
 
@@ -84,20 +94,33 @@ public class ReportedPostsController {
                     if (response.statusCode() == 200) {
                         try {
                             JsonNode respJson = new ObjectMapper().readTree(response.body());
+                            reportId = respJson.get("id").asText();
                             postId = respJson.get("postId").asText();
                             reason = respJson.get("reason").asText();
                         } catch (JsonProcessingException e) {
                             Platform.runLater(() ->
-                                showAlert("Ocurrió un error de nuestro lado, intente nuevamente", Alert.AlertType.ERROR)
+                                    showAlert("Ocurrió un error de nuestro lado, intente nuevamente", Alert.AlertType.ERROR)
                             );
                         }
                     } else if (response.statusCode() == 401) {
                         Platform.runLater(() ->
-                            showAlert("No está autorizado para acceder a esta información", Alert.AlertType.ERROR)
+                                showAlert("No está autorizado para acceder a esta información: 401", Alert.AlertType.ERROR)
+                        );
+                    } else if (response.statusCode() == 403) {
+                        Platform.runLater(() ->
+                                showAlert("No está autorizado para acceder a esta información: 403", Alert.AlertType.ERROR)
+                        );
+                    } else if (response.statusCode() == 404) {
+                        Platform.runLater(() ->
+                                showAlert("No se encontró el recurso", Alert.AlertType.ERROR)
                         );
                     } else if (response.statusCode() == 500) {
                         Platform.runLater(() ->
                             showAlert("Ocurrió un error de nuestro lado, intente más tarde", Alert.AlertType.ERROR)
+                        );
+                    } else {
+                        Platform.runLater(() ->
+                            showAlert("Ocurrió un error inesperado, intente más tarde. Código: " + response.statusCode(), Alert.AlertType.ERROR)
                         );
                     }
                 })
@@ -106,16 +129,17 @@ public class ReportedPostsController {
                         showAlert("Error de red" + ex.getMessage(), Alert.AlertType.ERROR)
                     );
                     return null;
-                });
+                }).join();
 
         if (postId == null) {
+            showAlert("no se obtuvo el id", Alert.AlertType.ERROR);
             return;
         }
 
         HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(URI.create(postsUri+"/"+postId))
                 .header("Content-Type", "application/json")
-                .header("Authorization", CurrentUser.jwt)
+                .header("Authorization", "Bearer " +CurrentUser.jwt)
                 .GET()
                 .build();
 
@@ -124,12 +148,14 @@ public class ReportedPostsController {
                     if (response.statusCode() == 200) {
                         try {
                             JsonNode respJson = new ObjectMapper().readTree(response.body());
-                            post.setID(respJson.get("_id").asText());
+                            post = new Post();
+                            post.setID(respJson.get("id").asText());
                             post.setTitle(respJson.get("title").asText());
                             post.setDescription(respJson.get("description").asText());
                             post.setCategory(respJson.get("category").asText());
                             post.setStatus(respJson.get("status").asText());
                             post.setTimestamp(respJson.get("timestamp").asText());
+                            post.setUserID(respJson.get("userId").asText());
                         } catch (JsonProcessingException e) {
                             Platform.runLater(() ->
                                 showAlert("Ocurrió un error de nuestro lado, intente nuevamente", Alert.AlertType.ERROR)
@@ -150,17 +176,24 @@ public class ReportedPostsController {
                         showAlert("Error de red" + ex.getMessage(), Alert.AlertType.ERROR)
                     );
                     return null;
-                });
+                }).join();
 
         if (post == null) {
+            showAlert("no se obtuvo el post", Alert.AlertType.ERROR);
             return;
         }
 
         MultimediaServiceManager manager = new MultimediaServiceManager();
         try {
             post.setMultimedia(manager.getPostMultimedia(post.getID()));
-        } catch (Error ex) {
+        } catch (Error | InterruptedException | IOException ex) {
             showAlert("No se pudo obtener la imagen o video del post", Alert.AlertType.ERROR);
+        }
+
+        if (post.getMultimedia() == null) {
+            showAlert("la imagen es nula", Alert.AlertType.ERROR);
+        } else if (post.getMultimedia().length == 0) {
+            showAlert("la imagen está vacía", Alert.AlertType.ERROR);
         }
     }
     
@@ -172,6 +205,7 @@ public class ReportedPostsController {
 
     public void deletePost(ActionEvent actionEvent) {
         if (post == null) {
+            showAlert("no se puede borrar la publicación no existente", Alert.AlertType.ERROR);
             return;
         }
 
@@ -190,8 +224,8 @@ public class ReportedPostsController {
         HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(URI.create(postsUri+"/"+postId))
                 .header("Content-Type", "application/json")
-                .header("Authorization", CurrentUser.jwt)
-                .method("Patch", HttpRequest.BodyPublishers.ofString(jsonBody, StandardCharsets.UTF_8))
+                .header("Authorization", "Bearer " +CurrentUser.jwt)
+                .method("Delete", HttpRequest.BodyPublishers.ofString(jsonBody, StandardCharsets.UTF_8))
                 .build();
 
         client.sendAsync(postRequest, HttpResponse.BodyHandlers.ofString())
@@ -209,19 +243,24 @@ public class ReportedPostsController {
                         Platform.runLater(() ->
                             showAlert("Ocurrió un error de nuestro lado, intente nuevamente", Alert.AlertType.ERROR)
                         );
+                    } else {
+                        Platform.runLater(() ->
+                                showAlert("Ocurrió un error de nuestro lado, intente nuevamente: código " + response.statusCode(), Alert.AlertType.ERROR)
+                        );
                     }
                 })
                 .exceptionally(ex -> {
                     return null;
-                });
+                }).join();
 
         if (post != null) {
+            showAlert("Error al borrar la publicación", Alert.AlertType.ERROR);
             return;
         }
 
         try {
             JsonNode payload = mapper.createObjectNode()
-                    .put("ok", "false");
+                    .put("ok", false);
             jsonBody = mapper.writeValueAsString(payload);
         } catch (Exception e) {
             showAlert("Internal error: " + e.getMessage(), Alert.AlertType.ERROR);
@@ -229,9 +268,9 @@ public class ReportedPostsController {
         }
 
         HttpRequest reportRequest = HttpRequest.newBuilder()
-                .uri(URI.create(reportsUri+"/"+postId))
+                .uri(URI.create(reportsUri+"/"+reportId))
                 .header("Content-Type", "application/json")
-                .header("Authorization", CurrentUser.jwt)
+                .header("Authorization", "Bearer " +CurrentUser.jwt)
                 .method("Patch", HttpRequest.BodyPublishers.ofString(jsonBody, StandardCharsets.UTF_8))
                 .build();
 
@@ -239,11 +278,15 @@ public class ReportedPostsController {
                 .thenAcceptAsync(response -> {
                     if (response.statusCode() == 401) {
                         Platform.runLater(() ->
-                                showAlert("No está autorizado para acceder a esta información", Alert.AlertType.ERROR)
+                            showAlert("No está autorizado para acceder a esta información", Alert.AlertType.ERROR)
                         );
                     } else if (response.statusCode() == 500) {
                         Platform.runLater(() ->
-                                showAlert("Ocurrió un error de nuestro lado, intente más tarde", Alert.AlertType.ERROR)
+                            showAlert("Ocurrió un error de nuestro lado, intente más tarde", Alert.AlertType.ERROR)
+                        );
+                    } else {
+                        Platform.runLater(() ->
+                            showAlert("Ocurrió un error de nuestro lado, intente más tarde: código " + response.statusCode(), Alert.AlertType.ERROR)
                         );
                     }
                 })
@@ -252,8 +295,9 @@ public class ReportedPostsController {
                             showAlert("Error de red" + ex.getMessage(), Alert.AlertType.ERROR)
                     );
                     return null;
-                });
+                }).join();
 
+        showAlert("Post borrado", Alert.AlertType.INFORMATION);
         resetPostInfo();
         nextReport();
     }
@@ -276,9 +320,9 @@ public class ReportedPostsController {
         }
 
         HttpRequest reportRequest = HttpRequest.newBuilder()
-                .uri(URI.create(reportsUri+"/"+postId))
+                .uri(URI.create(reportsUri+"/"+reportId))
                 .header("Content-Type", "application/json")
-                .header("Authorization", CurrentUser.jwt)
+                .header("Authorization", "Bearer " +CurrentUser.jwt)
                 .method("Patch", HttpRequest.BodyPublishers.ofString(jsonBody, StandardCharsets.UTF_8))
                 .build();
 
@@ -313,7 +357,7 @@ public class ReportedPostsController {
         HttpRequest postRequest = HttpRequest.newBuilder()
                 .uri(URI.create(postsUri+"/"+postId))
                 .header("Content-Type", "application/json")
-                .header("Authorization", CurrentUser.jwt)
+                .header("Authorization", "Bearer " +CurrentUser.jwt)
                 .method("Patch", HttpRequest.BodyPublishers.ofString(jsonBody, StandardCharsets.UTF_8))
                 .build();
 
--- src/main/java/com/betterme/managers/MultimediaServiceManager.java
+++ src/main/java/com/betterme/managers/MultimediaServiceManager.java
@@ -9,6 +9,8 @@ import io.grpc.stub.StreamObserver;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class MultimediaServiceManager {
     private final String multimediaUri = ProgramConfigurations.getConfiguration()
@@ -16,12 +18,13 @@ public class MultimediaServiceManager {
     private final String multimediaPort = ProgramConfigurations.getConfiguration()
             .getProperty("multimediaAPI.port");
 
-    public byte[] getPostMultimedia(String postId) throws Error {
+    public byte[] getPostMultimedia(String postId) throws Error, IOException, InterruptedException {
         ManagedChannel channel = ManagedChannelBuilder.forAddress(multimediaUri, Integer.parseInt(multimediaPort))
                 .usePlaintext().build();
         MultimediaServiceGrpc.MultimediaServiceStub stub = MultimediaServiceGrpc.newStub(channel);
 
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        final CountDownLatch finishLatch = new CountDownLatch(1);
 
         stub.getPostMultimedia(Multimedia.PostInfo.newBuilder().setId(postId).build(),
                 new StreamObserver<Multimedia.FileChunk>() {
@@ -30,6 +33,7 @@ public class MultimediaServiceManager {
                         try {
                             buffer.write(fileChunk.getChunk().toByteArray());
                         } catch (IOException e) {
+                            finishLatch.countDown();
                             throw new RuntimeException(e);
                         }
                     }
@@ -37,15 +41,28 @@ public class MultimediaServiceManager {
                     @Override
                     public void onError(Throwable throwable) {
                         channel.shutdown();
+                        finishLatch.countDown();
                         throw new Error(throwable.getMessage());
                     }
 
                     @Override
                     public void onCompleted() {
                         channel.shutdown();
+                        finishLatch.countDown();
                     }
                 });
 
-        return buffer.toByteArray();
+        if (!finishLatch.await(1, TimeUnit.MINUTES)) { // Espera máximo 10 minutos
+            System.err.println("La descarga no se completó en el tiempo esperado.");
+            throw new RuntimeException("Descarga de multimedia excedió el tiempo límite.");
+        }
+
+        try {
+            return buffer.toByteArray();
+        } finally {
+            if (!channel.isShutdown()) {
+                channel.shutdown();
+            }
+        }
     }
 }
